2edb2bf6fa7504fbb67824de1ebb6835e3ec7707
smruthuthi <smruthuthi@gmail.com>
2026-09-12 19:01:09 +0300

    Resolved the raised issues

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
 import os
 import logging
 import time
-import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def handle_all():
@@ -22,22 +21,21 @@ def handle_all():
         "timestamp": time.time(),
         "service_version": os.getenv('SERVICE_VERSION', '1.0.0')
     }
-    
-    # Add query parameters if present
+
     if request.args:
         response_data["query_params"] = dict(request.args)
-    
-    # Add request body if present
+
     if request.is_json:
         response_data["body"] = request.get_json()
     elif request.data:
         try:
             response_data["body"] = request.data.decode('utf-8')
-        except:
+        except UnicodeDecodeError:
             response_data["body"] = str(request.data)
-    
+
     logger.info(f"Request received: {request.method} {request.path}")
     return jsonify(response_data), 200
+
 
 @app.route('/health', methods=['GET'])
 def health_check():
@@ -48,6 +46,7 @@ def health_check():
         "service": "mock-service"
     }), 200
 
+
 @app.route('/echo', methods=['POST'])
 def echo():
     """Echo endpoint that returns the request body"""
@@ -57,6 +56,7 @@ def echo():
         "timestamp": time.time()
     }), 200
 
+
 @app.route('/status/<int:code>', methods=['GET'])
 def custom_status(code):
     """Return custom status code"""
@@ -65,6 +65,7 @@ def custom_status(code):
         "message": f"Returning status {code}",
         "timestamp": time.time()
     }), code
+
 
 @app.route('/delay/<int:seconds>', methods=['GET'])
 def delayed_response(seconds):
@@ -78,51 +79,6 @@ def delayed_response(seconds):
         "timestamp": time.time()
     }), 200
 
-@app.route('/Common/CurrencyExchangeRate/Get/2.1', methods=['GET', 'POST'])
-def currency_exchange_rate():
-    """SOAP endpoint for currency exchange rate"""
-    logger.info(f"Currency exchange rate request received - Method: {request.method}")
-    
-     
-    if request.method == 'GET':
-        return jsonify({
-            "endpoint": "/Common/CurrencyExchangeRate/Get/2.1",
-            "methods_allowed": ["GET", "POST"],
-            "description": "SOAP endpoint for currency exchange rate",
-            "test_command": "curl -X POST http://172.16.20.52/Common/CurrencyExchangeRate/Get/2.1 -H 'Content-Type: text/xml' -d '<?xml version=\"1.0\"?><test/>'",
-            "note": "Use POST method with XML content for SOAP response"
-        }), 200
-    
-    
-    if request.data:
-        logger.info(f"Request body: {request.data.decode('utf-8')}")
-    
-    soap_response = '''<?xml version="1.0" encoding="UTF-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns27="urn://co-opbank.co.ke/TS/Finacle/CurrencyExchangeRateList.1.0" xmlns:head="urn://co-opbank.co.ke/CommonServices/Data/Message/MessageHeader" xmlns:tns25="urn://co-opbank.co.ke/BS/Common/BSCurrencyExchangeRate.2.0" xmlns:ns10="urn://co-opbank.co.ke/Banking/CanonicalDataModel/ExchangeRate/2.0">
-   <soapenv:Header>
-      <head:ResponseHeader xmlns:tns26="urn://co-opbank.co.ke/BS/Account/CurrencyExchangeRate/Get/2.0" xmlns:tns3="urn://co-opbank.co.ke/CommonServices/Data/Common">
-         <tns3:CorrelationID>975b1168-564b-4b67-a983-8c8f327de9b4</tns3:CorrelationID>
-         <head:MessageID>3b319494-b302-4f05-9f18-2c3c94671e5c</head:MessageID>
-         <head:StatusCode>S_001</head:StatusCode>
-         <head:StatusDescription>Success</head:StatusDescription>
-         <head:StatusMessages>
-            <head:MessageCode>Y</head:MessageCode>
-            <head:MessageDescription>SUCCESS</head:MessageDescription>
-         </head:StatusMessages>
-      </head:ResponseHeader>
-   </soapenv:Header>
-   <soapenv:Body>
-      <tns25:ExchangeRateResponse>
-         <tns25:ExchangeRate>13.33356</tns25:ExchangeRate>
-         <tns25:FromCurrency>TZS</tns25:FromCurrency>
-         <tns25:ToCurrency>KES</tns25:ToCurrency>
-         <tns25:ConvertedAmount>14999.8</tns25:ConvertedAmount>
-         <tns25:MultiplyDivide>D</tns25:MultiplyDivide>
-      </tns25:ExchangeRateResponse>
-   </soapenv:Body>
-</soapenv:Envelope>'''
-    
-    return Response(soap_response, mimetype='text/xml'), 200
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
